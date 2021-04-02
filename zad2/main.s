94c3b9325cafca56331515ebb7d974d2f4a92cf2
makredzic <mak.redzic@gmail.com>
2021-04-02 10:58:09 +0200

    Zavrsen drugi zadatak.

--- zad2/main.s
+++ zad2/main.s
@@ -15,6 +15,7 @@ main:
   sw $t1, ($t2) # vrijednost iz $t1 saved na adresu iz $t2, to jest u result variablu
 
 
+
   sll $t1, $t0, 5 # $t1 = 32$t0 
   sll $t2, $t0, 3 # $t1 = 8$t0  
 
@@ -32,11 +33,31 @@ main:
   sw $t1, ($t2) #ostala u $t2 adresa od result variable
 
 
+
+  slti $t1, $t0, 8 #provjera da li je $t0(number) < 8, ako jeste, onda je number%8 = number 
+  beq $t1, $0, modulo1
+
+  addu $t1, $0, $t0
+  j endmodulo1
+
+modulo1:
   andi $t1, $t0, 7 #https://mziccard.me/2015/05/08/modulo-and-division-vs-bitwise-operations
+
+endmodulo1:
   sw $t1, ($t2)
 
-  andi $t1, $t0, 31 #ne radi jer je broj 15 < 31
+
+  slti $t1, $t0, 32 #identicno ko gore, samo sa 32
+  beq $t1, $0, modulo2
+
+  addu $t1, $0, $t0
+  j endmodulo2
+
+modulo2:
+  andi $t1, $t0, 31
+endmodulo2:
   sw $t1, ($t2)
+
 
   addiu $v0, $0, 0
   jr $ra
