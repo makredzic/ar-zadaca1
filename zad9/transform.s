7924b2c3f825bf1fe770bbf474b621c4386f20a3
Mak <74105530+makredzic@users.noreply.github.com>
2021-04-26 11:31:27 +0200

    Popravljen bug gdje je a0 sacuvan van stacka

--- zad9/transform.s
+++ zad9/transform.s
@@ -3,10 +3,10 @@
 transform: #a0 = char* buffer, $a1 = size, $a2 = *predicate
  
  addiu $sp, $sp, -64
- sw $a0, 64($sp) #vjerovatno je dovoljno samo a0 sacuvati jer
- sw $a1, 60($sp) #se on koristi prilikom poziva predicate i dolazi
- sw $a2, 56($sp) #do mijejanja a0 ali dobra praksa je i ostale argumente
- sw $ra, 52($sp) #sacuvati na stacku s obzirom da je ova transform funkcija i caller jer poziva predicate
+ sw $a0, 60($sp) #vjerovatno je dovoljno samo a0 sacuvati jer
+ sw $a1, 56($sp) #se on koristi prilikom poziva predicate i dolazi
+ sw $a2, 52($sp) #do mijejanja a0 ali dobra praksa je i ostale argumente
+ sw $ra, 48($sp) #sacuvati na stacku s obzirom da je ova transform funkcija i caller jer poziva predicate
 
  addiu $t0, $0, 0 # t0 = 0 = i
 
@@ -21,14 +21,14 @@ uslov:
   
   sb $v0, ($t2) # na adresu od buffer[i] spremimo return od predicate(buffer[i])
 
-  lw $a0, 64($sp)
-  lw $a1, 60($sp)
-  lw $a2, 56($sp)
+  lw $a0, 60($sp)
+  lw $a1, 56($sp)
+  lw $a2, 52($sp)
 
   addiu $t0, $t0, 1 # i++
   j uslov
 kraj:
-  lw $ra, 52($sp)
+  lw $ra, 48($sp)
   addiu $sp, $sp, 64
   jr $ra
   
